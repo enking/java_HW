a1e512538701962e2966f6a78bcaf9253d475ed1
enking <engur9902@gmail.com>
2024-07-31 18:29:37 +0900

    level2 where?

--- src/main/java/com/greedy/level02/normal/Application.java
+++ src/main/java/com/greedy/level02/normal/Application.java
@@ -1,2 +1,12 @@
-package com.greedy.level02.normal;public class Application {
+package com.greedy.level02.normal;
+
+public class Application {
+    public static void main(String[] args) {
+        RandomMaker app = new RandomMaker();
+
+        System.out.println(app.randomNumber(0,3));
+        System.out.println(app.randomUpperAlphabet(15));
+        System.out.println(app.rockPaperScissors());
+        System.out.println(app.tossCoin());
+    }
 }
--- src/main/java/com/greedy/level02/normal/RandomMaker.java
+++ src/main/java/com/greedy/level02/normal/RandomMaker.java
@@ -1,2 +1,43 @@
-package com.greedy.level02.normal;public class RandomMaker {
+package com.greedy.level02.normal;
+
+public class RandomMaker {
+    public int randomNumber(int min, int max) {
+        return (int) (Math.random() * (max - min) + min);
+    }
+    public String randomUpperAlphabet(int length){
+        StringBuilder result = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            char randomChar = (char) ('A' + Math.random() * 26);
+            result.append(randomChar);
+        }
+        return result.toString();
+    }
+    public String rockPaperScissors() {
+        int a = (int)(Math.random() * 3); // Generate random integer 0, 1, or 2
+        if (a == 0) {
+            return "바위";
+        } else if (a == 1) {
+            return "가위";
+        } else {
+            return "보";
+        }
+    }
+    public String tossCoin(){
+        int b = (int)(Math.random() * 2 );
+        if(b == 0){
+            return "앞면";
+        }
+        else return "뒷면";
+
+    }
+
+
 }
+
+
+/*
+* + randomNumber(min:int, max:int):int
++ randomUpperAlpabet(length:int):String
++ rockPaperScissors():String
++ tossCoin():String
+* */
